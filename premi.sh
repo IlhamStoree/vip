56340515437686a1262499e9cf62fba507a96bb1
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-10-19 15:55:42 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -18,8 +18,8 @@ NC='\e[0m'
 red='\e[1;31m'
 green='\e[0;32m'
 TIMES="10"
-CHATID="-1001973626140"
-KEY="5627879486:AAE0uDsbjwLGUVa9BjCDLU01Cd2F8xFpd8o"
+CHATID="-1813590936"
+KEY="6341004088:AAEl07D-R7EuxaQ72p-pTr4jUjtDmZccfFw"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 # ===================
 clear
